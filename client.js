3198f10d5e5715419c8cb0c2563aa7c86b8ff1f9
ghaliano <ghaliano2005@gmail.com>
2016-07-22 17:43:20 +0100

    adding scroll listener

--- client.js
+++ client.js
@@ -2,8 +2,13 @@
  * wraper for Meteor.subscribe
  * Inspired from https://github.com/Exartu/Exartu-Pagination
  */
-Meteor.pagerClient = function (collection, subscriptionName, filters, options) {
+Meteor.pagerClient = function (collection, subscriptionName, filters, options, settings) {
 	var self = this;
+	this._settings = _.extend({
+		fireAt: 100,
+		infiniteScroll: true
+	}, settings||{});
+
 	this._metadataCollection = new Meteor.Collection('pagerMetadata');
 	this._metadataCollectionHandler = Meteor.subscribe('pagerMetadata');
 
@@ -30,12 +35,13 @@ Meteor.pagerClient = function (collection, subscriptionName, filters, options) {
 		);
 
 		if (self._handler.ready() && self._metadataCollectionHandler.ready()){
-			
 			var metadata = self._metadataCollection.findOne(self._name);
 			if (metadata){
+				var currentCount = self._collection.find().count();
 				self._resultMetadata.set({
 					count: metadata.count,
-					hasMore: metadata.count>self._collection.find().count()
+					hasMore: metadata.count>currentCount,
+					pagesCount: Math.ceil(metadata.count/self.util.getLimit())
 				});
 			}
 			self._isLoading.set(false);
@@ -46,7 +52,7 @@ Meteor.pagerClient = function (collection, subscriptionName, filters, options) {
 	self.util = {
 		//Subscribe to more data
 		loadMore: function () {
-			self._options.set('limit', self._options_base.limit + self._options.get().limit);
+			self.util.setOption('limit', self._options_base.limit + self.util.getOption('limit'));
 		},
 		hasMore: function () {
 			return self._resultMetadata.get().hasMore;
@@ -59,45 +65,57 @@ Meteor.pagerClient = function (collection, subscriptionName, filters, options) {
 		isLoading: function () {
 			return self._isLoading.get();
 		},
-		//Add filter by index
-		addFilter: function(index, value) {
-			self._filters.set(index, value);
-		},
 		//Set filters
 		setFilters: function(filters) {
 			self._filters.set(filters);
 		},
 		//Get filter by index
-		getFilter: function (index) {
-			return self._filters.get()[index];
+		setFilter: function (index, value) {
+			var filters = self._filters.get();
+			filters[index] = value;
+			self.util.setOptions(filters);
+		},
+		getLimit: function (){
+			return self.util.getOption('limit');
 		},
 		//Set options
 		setOptions: function(options) {
 			self._options.set(options);
 		},
 		//add one option by index
-		addOption: function (index, value) {
-			self._options.set(index, value);
+		setOption: function (index, value) {
+			var options = self._options.get();
+			options[index] = value;
+			self.util.setOptions(options);
 		},
 		//Get option by index
 		getOption: function (index) {
 			return self._options.get()[index];
 		},
-		//Get current subscription results
-		getResults: function () {
-			return self._collection.find();
-		},
 		//Stop current subscription and reload
 		//Why reload? in some case like sorting where we need to resibscribe with custom sort.
 		stopAndReload: function () {
 			self._handler.stop();
-			var options = self._options.get();
-			options.limit = self._options_base.limit
-			self._options.set(options);
+			self.util.setOption('limit', self._options_base.limit);
 		},
 		//get total Cursor count (without limit)
 		getResultMetadata: function (index) {
 			return index?self._resultMetadata[index]:self._resultMetadata;
+		},
+		scrollListner: function(){
+			$(window).on("scroll", _.debounce(
+				function() {
+				  var elem = $(self._settings.container || window);
+				  if(elem.scrollTop() + elem.height() > $(self._settings.container || document).height() - self._settings.fireAt){
+				    self.util.loadMore();
+				  }
+				}, 
+				300
+			));
 		}
+	}
+
+	if (this._settings.infiniteScroll){
+		this.util.scrollListner();
 	}
 };
